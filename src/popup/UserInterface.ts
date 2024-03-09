a9a9acf6811d27c079941b59b42c2cefc7434b7a
Jack Le Hamster <jacklehamster@gmail.com>
2024-03-08 16:18:56 -0800

    Auto-commit: 2024-03-08 16:18:56
    Files:
    src/index.ts
    src/popup/UserInterface.ts
    
    https://github.com/jacklehamster/autocommit

--- src/popup/UserInterface.ts
+++ src/popup/UserInterface.ts
@@ -1,10 +1,13 @@
 import { MenuData } from "./menu/MenuData";
 import { DialogData } from "./dialog/DialogData";
+import { PopupListener } from "./base/PopupListener";
 
 export interface UserInterface {
   openMenu(menu: MenuData): Promise<void>;
   openDialog(dialog: DialogData): Promise<void>;
   closePopup(uid?: string): void;
   nextMessage(): void;
-  get selection(): number
+  get selection(): number;
+  addDialogListener(listener: PopupListener): void;
+  removeDialogListener(listener: PopupListener): void;
 }
